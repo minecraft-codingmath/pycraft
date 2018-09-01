659a1218df9d9f4a16fb18f71036af6ee44c887e
손량 <loop.infinitely@gmail.com>
2018-09-01 17:18:25 +0900

    fixed a bug in random_pits

--- src/api/examples/random_pits.py
+++ src/api/examples/random_pits.py
@@ -6,11 +6,11 @@ This script generates 1000 (default value) pits in x, z range -50..50.
 """
 
 
+import random
 import sys
 sys.path.append('..')
 
 from craft_api import CraftAPI
-import random
 
 api = CraftAPI()
 random.seed()
@@ -19,4 +19,4 @@ PIT_COUNT = 1000    # number of pits to create
 for _ in range(PIT_COUNT):
     x = random.randint(-50, 50)
     z = random.randint(-50, 50)
-    api.delete_block((x, -1, z))
+    api.delete_block((x, -2, z))
